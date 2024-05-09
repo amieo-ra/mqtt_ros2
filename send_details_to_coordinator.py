ffdec14ba74b1c1d850dda035e92ac93f2a2a6e2
amieo-ra <amieo@live.co.uk>
2024-05-09 10:48:22 +0100

    Adding scripts

---
+++ send_details_to_coordinator.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import tf
+import sys
+import os
+import json, yaml
+from pprint import pprint
+
+#import rospy
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, DurabilityPolicy
+
+#import rospkg
+from std_msgs.msg import String
+from geometry_msgs.msg import Pose
+from diagnostic_msgs.msg import KeyValue
+
+from gofar_navigation.msg import NewAgentConfig, Module
+
+class addagent(Node):
+
+    def __init__(self):
+        super().__init__('AddAgent')
+        self.publisher = self.create_publisher(NewAgentConfig, "/rasberry_coordination/dynamic_fleet/add_agent", 10)
+        timer_period = 0.25
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+
+        # Collect details
+        agent_id = os.getenv('ROBOT_NAME', 'gofar_001')
+        setup = os.getenv('AGENT_SETUP_CONFIG', None)
+
+        print("AddAgent Node launched")
+        print("Loading configurations:")
+        print("    - agent_file: %s"%agent_id)
+        print("    - setup_file: %s"%setup)
+
+        agent = self.load_agent_obj(agent_id, setup)
+        print("Details of Agent being launched:\n%s\n\n"%agent)
+
+        self.publisher.publish(agent)
+
+
+    def get_kvp_list(self, dict, item):
+        if item in dict:
+            return [KeyValue(k, str(v)) for k, v in dict[item].items()]
+        return []
+
+
+    def load_agent_obj(self, agent_id, setup_file, printer=True):
+
+        # Load file contents, (fallback on empty file if agent_file not found)
+        agent_data = {'agent_id': agent_id.split("/")[-1].split(".")[0]}
+        print("Launching with agent_data: %s" % (agent_data))
+
+        # Build msg
+        with open(setup_file) as f:
+            setup_data = yaml.safe_load(f)
+        pprint(setup_data)
+        print("\n")
+
+        # Construct object
+        agent = NewAgentConfig()
+        agent.agent_id = agent_data['agent_id']
+        agent.local_properties = self.get_kvp_list(agent_data, 'local_properties')
+        for m in setup_data['modules']:
+            m['details'] = m['details'] if 'details' in m else [{'key':'value'}]
+
+        agent.modules = [Module(m['name'], m['interface'], [KeyValue(list(d.keys())[0], yaml.dump(list(d.values())[0])) for d in m['details']]) for m in setup_data['modules']]
+        print("\n\n")
+        return agent
+
+
+if __name__ == '__main__':
+
+    rclpy.init(args=None)
+    AddAgent = addagent()
+    rclpy.spin(AddAgent)
+
+    AddAgent.destroy_node()
+    rclpy.shutdown()
+
